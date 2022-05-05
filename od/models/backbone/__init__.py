12c66c8953deee8eb881a9146bbcd292ad975e95
Jack Malloch <17jcm@queensu.ca>
2022-05-05 16:37:27 -0400

    ViT support.

--- od/models/backbone/__init__.py
+++ od/models/backbone/__init__.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 from .resnet import resnet
-from .shufflenetv2 import shufflenetv2
-from .mobilenetv3 import MobileNetV3 as mobilenetv3
 from .yolov5 import YOLOv5
-from .efficientnet import efficientnet
-from .hrnet import hrnet
 from .swin_transformer import swin_transformer as swin
+from .vit import vit 
 from .vgg import vgg
 from .repvgg import repvgg
 
 __all__ = ['build_backbone']
 
-support_backbone = ['resnet', 'shufflenetv2', 'mobilenetv3', 'YOLOv5', 'efficientnet', 'hrnet', 'swin', 'vgg', 'repvgg']
+support_backbone = ['resnet', 'YOLOv5', 'swin', 'vgg', 'vit']
 
 
 def build_backbone(backbone_name, **kwargs):
--- od/models/backbone/vit.py
+++ od/models/backbone/vit.py
@@ -1,2 +1,3 @@
 from pytorch_pretrained_vit import ViT
-model = ViT('B_16_imagenet1k', pretrained=True)
+def vit(pretrained=True):
+    return ViT('B_16_imagenet1k', pretrained=True)
