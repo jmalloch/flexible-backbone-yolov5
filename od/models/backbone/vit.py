22d11806a4419b923e9df90bca8e75febd1a8def
Jack Malloch <17jcm@queensu.ca>
2022-05-05 17:09:03 -0400

    More ViT support.

--- od/models/backbone/vit.py
+++ od/models/backbone/vit.py
@@ -1,3 +1,3 @@
 from pytorch_pretrained_vit import ViT
 def vit(pretrained=True):
-    return ViT('B_16_imagenet1k', pretrained=True)
+    return ViT('B_16_imagenet1k', pretrained=True, image_size=640)
